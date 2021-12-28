57062907cc3436936d8305a3dc6cdd7a77719439
DDU <ddu6@protonmail.com>
2021-12-28 15:31:25 +0800

    update

--- src/mod.ts
+++ src/mod.ts
@@ -1,7 +1,7 @@
 import type {Compiler,UnitCompiler} from '@ddu6/stc'
 const stuiVersion='0.6.0'
 const sthlVersion='0.7.0'
-const stStdVersion='0.15.2'
+const stStdVersion='0.15.3'
 export function removePlaceholders(string:string){
     return string.replace(/\n? *placeholder(\n|$)/g,'\n')
 }
