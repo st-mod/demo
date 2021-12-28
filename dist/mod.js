e6f0ede757338893b39b4c1e2c6663ec84a70073
DDU <ddu6@protonmail.com>
2021-12-29 00:22:28 +0800

    update

--- dist/mod.js
+++ dist/mod.js
@@ -1,6 +1,6 @@
 const stuiVersion = '0.6.0';
-const sthlVersion = '0.9.0';
-const stStdVersion = '0.16.0';
+const sthlVersion = '0.9.1';
+const stStdVersion = '0.16.1';
 export function removePlaceholders(string) {
     return string.replace(/\n? *placeholder(\n|$)/g, '\n');
 }
