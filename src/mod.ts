c66dd10043fcf8040a617d7460e81ed54b15428c
DDU <ddu6@protonmail.com>
2022-01-03 13:44:39 +0800

    update

--- src/mod.ts
+++ src/mod.ts
@@ -65,6 +65,8 @@ export const demo: UnitCompiler = async (unit, compiler) => {
     const root = resultEle.attachShadow({mode: 'open'})
     const style = document.createElement('style')
     const container = document.createElement('div')
+    source.classList.add('source')
+    resultEle.classList.add('result')
     element.append(source)
     element.append(resultEle)
     root.append(style)
