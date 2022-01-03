1fc46261f74208c83195871a177fbd8645c3cab9
DDU <ddu6@protonmail.com>
2022-01-03 12:11:15 +0800

    update

--- dist/mod.js
+++ dist/mod.js
@@ -1,6 +1,6 @@
-const stuiVersion = '0.7.6';
+const stuiVersion = '0.7.8';
 const sthlVersion = '0.12.2';
-const stStdVersion = '0.18.1';
+const stStdVersion = '0.18.3';
 export function removePlaceholders(string) {
     return string.replace(/\n? *placeholder(\n|$)/g, '\n');
 }
