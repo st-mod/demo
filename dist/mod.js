2fef132a4f093ff0b8fd2d8c4aca790515c06b67
DDU <ddu6@protonmail.com>
2022-01-19 17:44:58 +0800

    update

--- dist/mod.js
+++ dist/mod.js
@@ -1,4 +1,4 @@
-const stStdVersion = '0.25.3';
+const stStdVersion = '0.25.4';
 export function removePlaceholders(string) {
     return string.replace(/\n? *placeholder(\n|$)/g, '\n');
 }
--- src/mod.ts
+++ src/mod.ts
@@ -1,5 +1,5 @@
 import type {Compiler, UnitCompiler} from '@ddu6/stc'
-const stStdVersion = '0.25.3'
+const stStdVersion = '0.25.4'
 export function removePlaceholders(string: string) {
     return string.replace(/\n? *placeholder(\n|$)/g, '\n')
 }
