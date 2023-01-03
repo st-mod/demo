5b93b0e77fb56262d8c02ba5e23af59c6325a74f
DDU <ddu6@protonmail.com>
2023-01-03 17:09:40 +0800

    update

--- dist/mod.js
+++ dist/mod.js
@@ -1,62 +1,79 @@
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
 import { stringify as stonStringify } from 'ston/dist/stringify';
 import { stringify } from 'stdn/dist/stringify';
 const stStdVersion = '0.31.9';
 export function removePlaceholders(string) {
     return string.replace(/\n? *placeholder(\n|$)/g, '\n');
 }
-export async function createSourcePre(string, compiler) {
-    return await compiler.compileUnit({
-        tag: 'code',
-        options: {
-            lang: 'stdn',
-            block: true
-        },
-        children: string.split('\n').map(value => value.split(''))
+export function createSourcePre(string, compiler) {
+    return __awaiter(this, void 0, void 0, function* () {
+        return yield compiler.compileUnit({
+            tag: 'code',
+            options: {
+                lang: 'stdn',
+                block: true
+            },
+            children: string.split('\n').map(value => value.split(''))
+        });
     });
 }
-export async function createParsePre(string, compiler) {
-    return await compiler.compileUnit({
-        tag: 'code',
-        options: {
-            lang: 'ston',
-            block: true
-        },
-        children: stonStringify(compiler.stdn.parse(string), {
-            addDecorativeSpace: 'always',
-            indentTarget: 'all',
-            useUnquotedString: true
-        }).split('\n').map(value => value.split(''))
+export function createParsePre(string, compiler) {
+    return __awaiter(this, void 0, void 0, function* () {
+        return yield compiler.compileUnit({
+            tag: 'code',
+            options: {
+                lang: 'ston',
+                block: true
+            },
+            children: stonStringify(compiler.stdn.parse(string), {
+                addDecorativeSpace: 'always',
+                indentTarget: 'all',
+                useUnquotedString: true
+            }).split('\n').map(value => value.split(''))
+        });
     });
 }
-export async function shadowCompile(string, style, root, url, compiler) {
-    const stdURL = new URL(`../st-std@${stStdVersion}`, import.meta.url).href;
-    return await compiler.compile([{
-            value: string,
-            url
-        }], {
-        style,
-        headSTDN: [
-            [{
-                    tag: 'global', options: {
-                        'css-src': `${stdURL}/main.css`,
-                        'ucs-src': `${stdURL}/ucs.js`
-                    }, children: []
-                }]
-        ],
-        root
+export function shadowCompile(string, style, root, url, compiler) {
+    return __awaiter(this, void 0, void 0, function* () {
+        const stdURL = new URL(`../st-std@${stStdVersion}`, import.meta.url).href;
+        return yield compiler.compile([{
+                value: string,
+                url
+            }], {
+            style,
+            headSTDN: [
+                [{
+                        tag: 'global', options: {
+                            'css-src': `${stdURL}/main.css`,
+                            'ucs-src': `${stdURL}/ucs.js`
+                        }, children: []
+                    }]
+            ],
+            root
+        });
     });
 }
-export async function toHTMLPre(container, compiler) {
-    const pre = await compiler.compileSTDN([[{
-                tag: 'code',
-                options: {
-                    lang: 'html',
-                    block: true
-                },
-                children: container.innerHTML.split('\n').map(value => value.split(''))
-            }]]);
-    container.innerHTML = '';
-    container.append(pre);
+export function toHTMLPre(container, compiler) {
+    return __awaiter(this, void 0, void 0, function* () {
+        const pre = yield compiler.compileSTDN([[{
+                    tag: 'code',
+                    options: {
+                        lang: 'html',
+                        block: true
+                    },
+                    children: container.innerHTML.split('\n').map(value => value.split(''))
+                }]]);
+        container.innerHTML = '';
+        container.append(pre);
+    });
 }
 export function shadowHashAnchorsListener(e) {
     if (!(e.currentTarget instanceof Element)) {
@@ -84,7 +101,8 @@ export function shadowHashAnchorsListener(e) {
         return;
     }
 }
-export const demo = async (unit, compiler) => {
+export const demo = (unit, compiler) => __awaiter(void 0, void 0, void 0, function* () {
+    var _a, _b;
     const element = document.createElement('div');
     const source = document.createElement('div');
     const resultEle = document.createElement('div');
@@ -95,8 +113,48 @@ export const demo = async (unit, compiler) => {
     element.append(resultEle);
     let string = textarea.value = removePlaceholders(stringify(unit.children));
     let sourcePre;
-    if ((unit.options.parse ?? compiler.context.extractLastGlobalOption('parse', 'demo')) === true) {
-        async function render() {
+    if (((_a = unit.options.parse) !== null && _a !== void 0 ? _a : compiler.context.extractLastGlobalOption('parse', 'demo')) === true) {
+        function render() {
+            return __awaiter(this, void 0, void 0, function* () {
+                if (sourcePre !== undefined && textarea.value === string) {
+                    textarea.replaceWith(sourcePre);
+                    return false;
+                }
+                if (textarea.disabled) {
+                    return false;
+                }
+                textarea.disabled = true;
+                string = textarea.value;
+                const pre = sourcePre = yield createSourcePre(string, compiler);
+                source.innerHTML = '';
+                source.append(pre);
+                pre.addEventListener('click', () => {
+                    pre.replaceWith(textarea);
+                    textarea.disabled = false;
+                    textarea.focus();
+                });
+                resultEle.innerHTML = '';
+                resultEle.append(yield createParsePre(string, compiler));
+                return true;
+            });
+        }
+        yield render();
+        textarea.addEventListener('blur', () => __awaiter(void 0, void 0, void 0, function* () {
+            if (yield render()) {
+                element.dispatchEvent(new Event('adjust', { bubbles: true, composed: true }));
+            }
+        }));
+        return element;
+    }
+    const root = resultEle.attachShadow({ mode: 'open' });
+    const style = document.createElement('style');
+    const container = document.createElement('div');
+    root.append(style);
+    root.append(container);
+    const html = ((_b = unit.options.html) !== null && _b !== void 0 ? _b : compiler.context.extractLastGlobalOption('html', 'demo')) === true;
+    const url = compiler.context.urlToAbsURL('', unit);
+    function render() {
+        return __awaiter(this, void 0, void 0, function* () {
             if (sourcePre !== undefined && textarea.value === string) {
                 textarea.replaceWith(sourcePre);
                 return false;
@@ -106,7 +164,7 @@ export const demo = async (unit, compiler) => {
             }
             textarea.disabled = true;
             string = textarea.value;
-            const pre = sourcePre = await createSourcePre(string, compiler);
+            const pre = sourcePre = yield createSourcePre(string, compiler);
             source.innerHTML = '';
             source.append(pre);
             pre.addEventListener('click', () => {
@@ -114,71 +172,36 @@ export const demo = async (unit, compiler) => {
                 textarea.disabled = false;
                 textarea.focus();
             });
-            resultEle.innerHTML = '';
-            resultEle.append(await createParsePre(string, compiler));
-            return true;
-        }
-        await render();
-        textarea.addEventListener('blur', async () => {
-            if (await render()) {
-                element.dispatchEvent(new Event('adjust', { bubbles: true, composed: true }));
+            const result = yield shadowCompile(string, style, root, url, compiler);
+            if (result === undefined) {
+                return true;
             }
-        });
-        return element;
-    }
-    const root = resultEle.attachShadow({ mode: 'open' });
-    const style = document.createElement('style');
-    const container = document.createElement('div');
-    root.append(style);
-    root.append(container);
-    const html = (unit.options.html ?? compiler.context.extractLastGlobalOption('html', 'demo')) === true;
-    const url = compiler.context.urlToAbsURL('', unit);
-    async function render() {
-        if (sourcePre !== undefined && textarea.value === string) {
-            textarea.replaceWith(sourcePre);
-            return false;
-        }
-        if (textarea.disabled) {
-            return false;
-        }
-        textarea.disabled = true;
-        string = textarea.value;
-        const pre = sourcePre = await createSourcePre(string, compiler);
-        source.innerHTML = '';
-        source.append(pre);
-        pre.addEventListener('click', () => {
-            pre.replaceWith(textarea);
-            textarea.disabled = false;
-            textarea.focus();
-        });
-        const result = await shadowCompile(string, style, root, url, compiler);
-        if (result === undefined) {
+            container.innerHTML = '';
+            container.append(result.documentFragment);
+            if (html) {
+                yield toHTMLPre(container, result.compiler);
+                return true;
+            }
             return true;
-        }
-        container.innerHTML = '';
-        container.append(result.documentFragment);
-        if (html) {
-            await toHTMLPre(container, result.compiler);
-            return true;
-        }
-        return true;
+        });
     }
-    await render();
-    textarea.addEventListener('blur', async () => {
-        if (await render()) {
+    yield render();
+    textarea.addEventListener('blur', () => __awaiter(void 0, void 0, void 0, function* () {
+        if (yield render()) {
             element.dispatchEvent(new Event('adjust', { bubbles: true, composed: true }));
         }
-    });
+    }));
     container.addEventListener('click', shadowHashAnchorsListener);
     return element;
-};
-export const source = async (unit, compiler) => {
-    return await createSourcePre(removePlaceholders(stringify(unit.children)), compiler);
-};
-export const result = async (unit, compiler) => {
+});
+export const source = (unit, compiler) => __awaiter(void 0, void 0, void 0, function* () {
+    return yield createSourcePre(removePlaceholders(stringify(unit.children)), compiler);
+});
+export const result = (unit, compiler) => __awaiter(void 0, void 0, void 0, function* () {
+    var _c, _d;
     const element = document.createElement('div');
-    if ((unit.options.parse ?? compiler.context.extractLastGlobalOption('parse', 'result')) === true) {
-        element.append(await createParsePre(stringify(unit.children), compiler));
+    if (((_c = unit.options.parse) !== null && _c !== void 0 ? _c : compiler.context.extractLastGlobalOption('parse', 'result')) === true) {
+        element.append(yield createParsePre(stringify(unit.children), compiler));
         return element;
     }
     const root = element.attachShadow({ mode: 'open' });
@@ -186,15 +209,15 @@ export const result = async (unit, compiler) => {
     const container = document.createElement('div');
     root.append(style);
     root.append(container);
-    const result = await shadowCompile(stringify(unit.children), style, root, compiler.context.urlToAbsURL('', unit), compiler);
+    const result = yield shadowCompile(stringify(unit.children), style, root, compiler.context.urlToAbsURL('', unit), compiler);
     if (result === undefined) {
         return element;
     }
     container.append(result.documentFragment);
-    if ((unit.options.html ?? compiler.context.extractLastGlobalOption('html', 'result')) === true) {
-        await toHTMLPre(container, result.compiler);
+    if (((_d = unit.options.html) !== null && _d !== void 0 ? _d : compiler.context.extractLastGlobalOption('html', 'result')) === true) {
+        yield toHTMLPre(container, result.compiler);
         return element;
     }
     container.addEventListener('click', shadowHashAnchorsListener);
     return element;
-};
+});
